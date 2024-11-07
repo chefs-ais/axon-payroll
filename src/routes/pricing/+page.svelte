32d7eb396ef479a65d3988a153d3c7617b13aedc
Yaa-Henewaa <maameyaahenewaa2001@gmail.com>
2024-11-06 23:54:16 -1200

    pricing update

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -3,17 +3,17 @@
 
 	const pricing = [
 		{
-			packageName: ' Free',
-			price: '$0',
-			description: ' Up to 15 years',
+			packageName: 'Admin Package',
+			price: 'GHC 0 /month',
+			description: 'Package for system only',
 			features: ['All features included', 'Community suport channel', 'Multi-level permissions'],
-			button: 'Get Started',
+			button: 'SUBSCRIBE',
 			isFeaturedPrice: false
 		},
 		{
-			packageName: ' Pro',
-			price: '$30 per month/dev',
-			description: ' Minimum $18,000 annually',
+			packageName: 'Paystack Pack One',
+			price: 'GHC 20 /month',
+			description: 'testing',
 			features: [
 				'All features included',
 				'Community suport channel',
@@ -21,35 +21,35 @@
 				'Limited to one instance',
 				'SDD'
 			],
-			button: 'Buy Now',
+			button: 'SUBSCRIBE',
 			isFeaturedPrice: true
 		},
-		{
-			packageName: ' Enterprise',
-			price: 'Custom',
-			description: ' Minimum $18,000 annually',
-			features: [
-				'All features included',
-				'Community suport channel',
-				'Multi-level permissions',
-				'Limited to one instance',
-				'SDD'
-			],
-			button: "Let's Talk",
-			isFeaturedPrice: false
-		}
+		// {
+		// 	packageName: ' Enterprise',
+		// 	price: 'Custom',
+		// 	description: ' Minimum $18,000 annually',
+		// 	features: [
+		// 		'All features included',
+		// 		'Community suport channel',
+		// 		'Multi-level permissions',
+		// 		'Limited to one instance',
+		// 		'SDD'
+		// 	],
+		// 	button: "Let's Talk",
+		// 	isFeaturedPrice: false
+		// }
 	];
 </script>
 
 <div class="py-16">
-	<div class="mx-auto flex max-w-screen-xl justify-center p-5 text-6xl font-bold">
-		Pricing Plans
+	<div class="mx-auto flex max-w-screen-xl justify-center  p-5 text-6xl font-bold ">
+		All-in-one platform
 	</div>
-	<div class="mx-auto flex max-w-screen-xl justify-center p-2 text-lg">
-		Choose the Plan That Fits Your Needs
+	<div class="mx-auto flex max-w-screen-xl justify-center  p-2 text-lg ">
+		You take care of your companies and we take care of the organization and collaboration and everything else
 	</div>
 
-	<div class="mx-auto grid max-w-screen-xl grid-cols-3 divide-x divide-gray-400 p-5">
+<div class="mx-auto grid max-w-screen-xl lg:grid-cols-2 md:grid-cols-1 md:divide-x-0	lg:divide-x divide-gray-400 p-5 place-items-center">
 		{#each pricing as price_item}
 			<PricingCard
 				CardData={{
