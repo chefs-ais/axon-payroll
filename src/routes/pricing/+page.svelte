1c7497de50c00721c9661e51f15216d77d8b1a94
Yaa-Henewaa <maameyaahenewaa2001@gmail.com>
2024-11-08 14:01:39 -1200

    pricing page re-make

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -4,7 +4,7 @@
 	const pricing = [
 		{
 			packageName: 'Admin Package',
-			price: 'GHC 0 /month',
+			price: '₵0',
 			description: 'Package for system only',
 			features: ['All features included', 'Community suport channel', 'Multi-level permissions'],
 			button: 'SUBSCRIBE',
@@ -12,7 +12,7 @@
 		},
 		{
 			packageName: 'Paystack Pack One',
-			price: 'GHC 20 /month',
+			price: '₵20',
 			description: 'testing',
 			features: [
 				'All features included',
@@ -23,35 +23,35 @@
 			],
 			button: 'SUBSCRIBE',
 			isFeaturedPrice: true
+		},
+		{
+			packageName: ' Enterprise',
+			price: 'Custom',
+			description: ' Minimum $18,000 annually',
+			features: [
+				'All features included',
+				'Community suport channel',
+				'Multi-level permissions',
+				'Limited to one instance',
+				'SDD'
+			],
+			button: "Let's Talk",
+			isFeaturedPrice: false
 		}
-		// {
-		// 	packageName: ' Enterprise',
-		// 	price: 'Custom',
-		// 	description: ' Minimum $18,000 annually',
-		// 	features: [
-		// 		'All features included',
-		// 		'Community suport channel',
-		// 		'Multi-level permissions',
-		// 		'Limited to one instance',
-		// 		'SDD'
-		// 	],
-		// 	button: "Let's Talk",
-		// 	isFeaturedPrice: false
-		// }
 	];
 </script>
 
 <div class="py-16">
-	<div class="mx-auto max-w-screen-xl">
+	<div class="mx-auto max-w-screen-xl p-5">
 		<div class="text-center">
-			<div class=" justify-center text-6xl font-bold tracking-tight">All-in-one platform</div>
+			<div class=" justify-center text-6xl font-bold tracking-tight  sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl ">All-in-one platform</div>
 			<div class=" justify-center text-lg mt-2">
 				You take care of your companies and we take care of the organization and collaboration and
 				everything else
 			</div>
 		</div>
 		<div
-			class="grid grid-cols-1 md:grid-cols-2 md:divide-x gap-5"
+			class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-screen-xl mx-auto mt-12"
 		>
 			{#each pricing as price_item}
 				<PricingCard
